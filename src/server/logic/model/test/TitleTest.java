5460ba2f45b6f5668901ee9d7bf8ba0a3101a514
Ali Hassam <alihassam@Alis-MacBook-Pro.local>
2017-10-08 23:32:46 -0400

    added TitleConstructor Test which passed

---
+++ src/server/logic/model/test/TitleTest.java
@@ -0,0 +1,20 @@
+package server.logic.model.test;
+
+import server.logic.model.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TitleTest {
+
+
+	@Test
+	public void TitleConstructorPassTesting(){
+		
+		assertNotNull(new Title ("23456789987","Mobi Dick"));	
+		
+	}
+	
+	
+
+}
